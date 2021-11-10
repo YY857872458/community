54b4bf5c8b641c4738adf71236fd6b395e1401ec
Yang Yang2e <yang.yang2@thoughtworks.comm>
2021-11-10 15:22:33 +0800

    init repo

---
+++ src/main/java/life/yang/community/studycommunity/StudyCommunityApplication.java
@@ -0,0 +1,13 @@
+package life.yang.community.studycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/yang/community/studycommunity/StudyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.yang.community.studycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
