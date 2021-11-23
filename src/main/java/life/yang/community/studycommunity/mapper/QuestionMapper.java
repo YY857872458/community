c0c8355328cf85b290cde58899810fef0032919a
YY857872458 <857872458@qq.com>
2021-11-23 10:23:11 +0800

    change mybatis annotation to xml

--- src/main/java/life/yang/community/studycommunity/mapper/QuestionMapper.java
+++ src/main/java/life/yang/community/studycommunity/mapper/QuestionMapper.java
@@ -5,31 +5,27 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
 
-    @Insert("insert into question (id,title,description,create_at,modified_at,creator,comment_count,view_count,like_count,tag)" +
-            "values (#{id},#{title},#{description},#{createAt},#{modifiedAt},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
     void create(Question question);
 
-    @Select("select * from question limit #{offset},#{size}")
     List<Question> list(@Param(value = "offset") Integer offset,
                         @Param(value = "size") Integer size);
 
-    @Select("select count(*) from question")
     Integer count();
 
-    @Select("select * from question where creator = #{userId} limit #{offset},#{size}")
     List<Question> listByUserId(@Param("userId") Long userId,
                                 @Param("offset") Integer offset,
                                 @Param("size") Integer size);
 
-    @Select("select count(*) from question where creator = #{userId}")
     Integer countById(@Param("userId") Long userId);
 
-    @Select("select * from question where id = #{id}")
-    Question findById(@Param("id") String id);
+    Question findById(@Param("id") Long id);
+
+    void update(Question question);
 }
--- src/main/java/life/yang/community/studycommunity/mapper/UserMapper.java
+++ src/main/java/life/yang/community/studycommunity/mapper/UserMapper.java
@@ -10,19 +10,13 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface UserMapper {
 
-    @Insert("insert into user (account_id,name,token,create_at,modified_at,bio,avatar_url) " +
-            "values (#{accountId},#{name},#{token},#{createAt},#{modifiedAt},#{bio},#{avatarUrl})")
     void insert(User user);
 
-    @Select("select * from user where token = #{token}")
     User findByToken(String token);
 
-    @Select("select * from user where id = #{id}")
     User findById(@Param("id") Long id);
 
-    @Select("select * from user where accountId = #{accountId}")
     User findByAccountId(@Param("accountId") String accountId);
 
-    @Update("update user set name = #{name}, token = #{token}, modified_at=#{modifiedAt}, bio = #{bio} where id = #{id}")
     void update(User dbUser);
 }
